b78f282c7572862fbc42ac2f96760405676d4fbd
yuranasdfsdafa <u.ranesdafdasfasdy@kakao.com>
2023-04-09 11:56:28 +0900

    4.9

--- ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-04.hoisting.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-04.hoisting.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
 
 
@@ -9,4 +11,25 @@
   함수의 호이스팅
 
 */
-"use strict";
+// 변수의 호이스팅
+console.log(aaa); // 변수의 사용 === undefined
+var aaa = '변수의 호이스팅';
+console.log(aaa); // 변수의 사용 === 변수의 호이스팅
+
+var bbb = '변수의 호이스팅2';
+console.log(bbb);
+var msg = 'global scope';
+if (true) {
+  var msg = 'block scope';
+}
+var msg2 = 'global scope';
+if (true) {
+  var _msg = 'block scope';
+}
+
+// 함수의 호이스팅
+debugger;
+console.log(add(1, 2)); // 3
+var add = function add(a, b) {
+  return a + b;
+};
--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-04.hoisting.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-04.hoisting.js
@@ -9,4 +9,27 @@
   함수의 호이스팅
 
 */
+// 변수의 호이스팅
+console.log(aaa); // 변수의 사용 === undefined
+var aaa = '변수의 호이스팅';
+console.log(aaa); // 변수의 사용 === 변수의 호이스팅
 
+const bbb = '변수의 호이스팅2';
+console.log(bbb);
+
+var msg = 'global scope';
+if (true) {
+  var msg = 'block scope';
+}
+
+let msg2 = 'global scope';
+if (true) {
+  let msg2 = 'block scope';
+}
+
+// 함수의 호이스팅
+debugger;
+console.log(add(1, 2)); // 3
+const add = (a, b) => {
+  return a + b;
+};
