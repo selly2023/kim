d33cedff1937286a88b37baad95186c94ccff565
yuranasdfsdafa <u.ranesdafdasfasdy@kakao.com>
2023-04-16 17:15:55 +0900

    4.16

---
+++ React/rt03.Webpack/webpackprj01/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header({ ...props }) {
+  return (
+    <div w3-include-header="header.html">
+      <header data-role="header">
+        <h1>Header.Component</h1>
+      </header>
+    </div>
+  );
+}
+export default Header;
