222c22252606f39444b4ef42789c298a42c05f36
yuranasdfsdafa <u.ranesdafdasfasdy@kakao.com>
2023-04-23 15:11:37 +0900

    4.23

--- React/rt20.create-reate-project/reactprj01/src/App.js
+++ React/rt20.create-reate-project/reactprj01/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import CompStyle from './containers/styled/CompStyle';
-import CrudContainer from './containers/crud/CrudContainer';
 import PageHome from './pages/PageHome';
+import PageLogin from './pages/PageLogin';
 
 function App() {
   return (
